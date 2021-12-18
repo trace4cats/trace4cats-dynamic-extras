c1f146af9741f651b7ce94030d6e7ac4ceba5f85
catostrophe <40268503+catostrophe@users.noreply.github.com>
2021-12-18 21:08:47 +0300

    feat: remove BlazeServer, let users provide ServerBuilder
    Ref: trace4cats/trace4cats-dynamic-extras#52

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -21,12 +21,11 @@ object Dependencies {
   lazy val trace4catsModel = "io.janstenpickle"   %% "trace4cats-model"                   % Versions.trace4cats
   lazy val trace4catsTestkit = "io.janstenpickle" %% "trace4cats-testkit"                 % Versions.trace4cats
 
-  lazy val circeGeneric = "io.circe"        %% "circe-generic"       % Versions.circe
-  lazy val http4sCirce = "org.http4s"       %% "http4s-circe"        % Versions.http4s
-  lazy val http4sCore = "org.http4s"        %% "http4s-core"         % Versions.http4s
-  lazy val http4sDsl = "org.http4s"         %% "http4s-dsl"          % Versions.http4s
-  lazy val http4sBlazeServer = "org.http4s" %% "http4s-blaze-server" % Versions.http4s
-  lazy val http4sServer = "org.http4s"      %% "http4s-server"       % Versions.http4s
+  lazy val circeGeneric = "io.circe"   %% "circe-generic" % Versions.circe
+  lazy val http4sCirce = "org.http4s"  %% "http4s-circe"  % Versions.http4s
+  lazy val http4sCore = "org.http4s"   %% "http4s-core"   % Versions.http4s
+  lazy val http4sDsl = "org.http4s"    %% "http4s-dsl"    % Versions.http4s
+  lazy val http4sServer = "org.http4s" %% "http4s-server" % Versions.http4s
 
   lazy val kindProjector = ("org.typelevel" % "kind-projector"     % Versions.kindProjector).cross(CrossVersion.full)
   lazy val betterMonadicFor = "com.olegpy" %% "better-monadic-for" % Versions.betterMonadicFor
